d00da4dd28b72eb85e5036bf1863f07e2a3dcaf1
AntonFrontendDev <antonfedorov2025@mail.ru>
2024-12-03 06:02:31 +1000

    fix error lint storybook

--- config/storybook/webpack.config.ts
+++ config/storybook/webpack.config.ts
@@ -3,6 +3,7 @@ import { BuildPaths } from '../build/types/config';
 import path from 'path';
 import { buildCssLoader } from '../build/loaders/buildCssLoader';
 import { buildSvgLoader } from '../build/loaders/buildSvgLoader';
+
 export default ({ config }: { config: webpack.Configuration }) => {
     const paths: BuildPaths = {
         build: '',
