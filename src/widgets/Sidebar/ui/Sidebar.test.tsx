08ec634fe5c373321d1f9b64056e8b8339a26c26
AntonFrontendDev <antonfedorov2025@mail.ru>
2024-12-03 19:18:45 +1000

    fix screens

--- src/widgets/Sidebar/ui/Sidebar.test.tsx
+++ src/widgets/Sidebar/ui/Sidebar.test.tsx
@@ -1,4 +1,4 @@
-import { fireEvent, render, screen } from '@testing-library/react';
+import { fireEvent, screen } from '@testing-library/react';
 import { Sidebar } from './Sidebar';
 import { renderComponent } from '@/shared/lib/tests/renderComponent/renderComponent';
 
